b2d702ce2174606b6e4b5ecb2740e777b5750cd2
l4mberjack <frolov_dmitriy06@mail.ru>
2024-11-16 00:17:54 +0300

    init1

--- src/Point.kt
+++ src/Point.kt
@@ -4,4 +4,5 @@ class Point (val x: Double, val y: Double) {
     fun distancePoint(other: Point): Double {
         return sqrt((x - other.x).pow(2) + (y - other.y).pow(2))
     }
+
 }
--- src/task3.kt
+++ src/task3.kt
@@ -1,14 +1,29 @@
+
 fun main(){
+    val points = arrayListOf<Point>()
     println("Задайте количество точек на плоскости:")
     val count = readln().toInt()
     var counter = 0
-    while(counter != count){
+    for(i in 0 until count){
         println("Введите точку ${counter+1}")
-        val p = Point(readln().toDouble(), readln().toDouble())
-        println(p)
+        val x = Math.random() * 10.0
+        val y = Math.random() * 10.0
+        points.add(Point(x, y))
         counter++
     }
+    var minDistance = Double.MAX_VALUE
+    var maxDistance = Double.MIN_VALUE
 
+    for (i in 0 until count) {
+        for (j in i + 1 until count) { // Избегаем повторного сравнения и сравнения точки с собой
+            val distance = points[i].distancePoint(points[j])
+            minDistance = minOf(minDistance, distance)
+            maxDistance = maxOf(maxDistance, distance)
+        }
+    }
+
+    println("Минимальное расстояние между точками: $minDistance")
+    println("Максимальное расстояние между точками: $maxDistance")
 
 
 
